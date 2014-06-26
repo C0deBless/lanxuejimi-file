89b1768f8f8ef89ec38780f3a7fa595830fc3685
minqingxiu527 <minqingxiu527@gmail.com>
2014-06-26 12:54:56 +0000

    添加  开炮的 功能

--- Java/TankCommon/src/common/Missile.java
+++ Java/TankCommon/src/common/Missile.java
@@ -1,134 +1,173 @@
 package common;
+
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.nio.ByteBuffer;
 import java.util.List;
 
 public class Missile {
-	public static final int XSPEED = 10;
-	public static final int YSPEED = 10;
-	public static final int WIDTH = 7;
-	public static final int HEIGHT = 7;
+	
 
-	public static int ID = 1;
+	public static int missileIndex = 0;
 
-	private int x, y;
-	private Direction dir;
+	private float x;
+	private float y;
+	
+	private int width = 6;
+	private int height = 6;
+	
+	private float missileSpeed;
+
+	private int angle;
+
+	private int team;
+
 	private boolean live = true;
 	private boolean good;
 
-	private int tankId;
 	private int id;
 
-	public Missile(int tankId, int x, int y, boolean good, Direction dir) {
-		this(tankId, x, y, dir);
-		this.good = good;
-		this.id = ID++;
-	}
-
-	public Missile(int tankId, int x, int y, Direction dir) {
-		this.tankId = tankId;
+	public Missile(float x, float y, int angle, int team, int id) {
 		this.x = x;
 		this.y = y;
-		this.dir = dir;
+		this.angle = angle;
+		this.team = team;
+		this.id = id;
 
 	}
 
-//	public void darw(Graphics g) {
-//		if (!live) {
-//			tc.missiles.remove(this);
-//			return;
-//		}
-//		Color c = g.getColor();
-//		if (good) {
-//			g.setColor(Color.PINK);
-//		} else {
-//			g.setColor(Color.GREEN);
-//		}
-//		g.fillOval(x, y, 7, 7);
-//		g.setColor(c);
-//
-//	}
-//
-//	public boolean hitTank(Tank t) {
-//		if (getRectangle().intersects(t.getRectangle()) && t.isLive()
-//				&& t.isGood() != good) {
-//			live = false;
-//			t.setLive(false);
-//			Explode e = new Explode(x, y, tc);
-//			tc.explode.add(e);
-//			return true;
-//		}
-//		return false;
-//	}
+	public Missile(float x, float y, int angle, int team) {
+		this.x = x;
+		this.y = y;
+		this.angle = angle;
+		this.team = team;
+		this.id = (++missileIndex);
+	}
 
-//	public boolean hitTanks(List<Tank> tanks) {
-//		for (int i = 0; i < tanks.size(); i++) {
-//			Tank t = tanks.get(i);
-//			hitTank(t);
-//		}
-//
-//		return false;
-//	}
+	public Missile(float x, float y, int angle) {
+		this.x = x;
+		this.y = y;
+		this.angle = angle;
+	}
+
+	public void update(long deltaTime) {
+		int factorX = 0;
+		int factorY = 0;
+
+		switch (angle) {
+		case 0:
+			factorY = -2;
+			break;
+		case 1:
+			factorX = 2;
+			break;
+		case 2:
+			factorY =2;
+			break;
+		case 3:
+			factorX = -2;
+			break;
+		}
+
+		float deltaPos = missileSpeed * (deltaTime / 1000.0f);
+
+		x += deltaPos * factorX;
+		y += deltaPos * factorY;
+	}
+	
+	public void serialize(ByteBuffer buffer) {
+		buffer.putInt(id);
+		buffer.putFloat(x);
+		buffer.putFloat(y);
+		buffer.putFloat(missileSpeed);
+		buffer.putInt(angle);
+		buffer.putInt(width);
+		buffer.putInt(height);
+		buffer.putInt(team);
+	}
+	
+	public static Missile deserialize(ByteBuffer buffer) {
+		int id = buffer.getInt();
+		float x = buffer.getFloat();
+		float y = buffer.getFloat();
+		float currentSpeed = buffer.getFloat();
+		int angle = buffer.getInt();
+		int width = buffer.getInt();
+		int height = buffer.getInt();
+		int team = buffer.getInt();
+		
+		Missile missile = new Missile(x, y, angle, team, id);
+		missile.missileSpeed = currentSpeed;
+		missile.width = width;
+		missile.height = height;
+		return missile;
+	}
+
+	// public void darw(Graphics g) {
+	// if (!live) {
+	// tc.missiles.remove(this);
+	// return;
+	// }
+	// Color c = g.getColor();
+	// if (good) {
+	// g.setColor(Color.PINK);
+	// } else {
+	// g.setColor(Color.GREEN);
+	// }
+	// g.fillOval(x, y, 7, 7);
+	// g.setColor(c);
+	//
+	// }
+	//
+	// public boolean hitTank(Tank t) {
+	// if (getRectangle().intersects(t.getRectangle()) && t.isLive()
+	// && t.isGood() != good) {
+	// live = false;
+	// t.setLive(false);
+	// Explode e = new Explode(x, y, tc);
+	// tc.explode.add(e);
+	// return true;
+	// }
+	// return false;
+	// }
+
+	// public boolean hitTanks(List<Tank> tanks) {
+	// for (int i = 0; i < tanks.size(); i++) {
+	// Tank t = tanks.get(i);
+	// hitTank(t);
+	// }
+	//
+	// return false;
+	// }
 
 	public Rectangle getRectangle() {
-		return new Rectangle(x, y, WIDTH, HEIGHT);
+		return new Rectangle((int)x, (int)y, width, height);
 	}
 
-	public boolean isLive() {
-		return live;
+	public void setMissileSpeed(float missileSpeed) {
+		this.missileSpeed = missileSpeed;
 	}
 
-	public int getX() {
+	public int getTeam() {
+		return team;
+	}
+
+	public float getX() {
 		return x;
 	}
 
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public int getY() {
+	public float getY() {
 		return y;
 	}
 
-	public void setY(int y) {
-		this.y = y;
+	public int getWidth() {
+		return width;
 	}
 
-	public Direction getDir() {
-		return dir;
+	public int getHeight() {
+		return height;
 	}
 
-	public void setDir(Direction dir) {
-		this.dir = dir;
-	}
-
-	public int getTankId() {
-		return tankId;
-	}
-
-	public void setTankId(int tankId) {
-		this.tankId = tankId;
-	}
-
-	public boolean isGood() {
-		return good;
-	}
-
-	public void setGood(boolean good) {
-		this.good = good;
-	}
-
-	public void setLive(boolean live) {
-		this.live = live;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
+	
 }
