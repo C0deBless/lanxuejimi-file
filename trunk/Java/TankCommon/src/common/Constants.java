b4bd114e5faaccb5fee99b0185ee0f9eed36254d
minqingxiu527@gmail.com <minqingxiu527@gmail.com@847ceae0-d93b-63e0-dcdd-eb63285fa432>
2014-07-13 08:19:14 +0000

    解决题图大小的问题

--- trunk/Java/TankCommon/src/common/Constants.java
+++ trunk/Java/TankCommon/src/common/Constants.java
@@ -2,14 +2,14 @@ package common;
 
 public class Constants {
 	
-	public static final int GAME_WIDTH = 500;
-	public static final int GAME_HEIGHT = 500;
+	public static final int GAME_WIDTH = 468;
+	public static final int GAME_HEIGHT = 468;
 
 	public static final int WINDOW_WIDTH = 800;
 	public static final int WINDOW_HEIGHT = 600;
 	
-	public static final int CAMP_X = 232;
-	public static final int CAMP_Y =464;
+	public static final int CAMP_X = 216;
+	public static final int CAMP_Y =432;
 	
-	public static final int A_GRID = 37;
+	public static final int A_GRID = 36;
 }
